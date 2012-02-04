273ca1d4217c39ef9eec8d9105fb12673a4f2df4
JamesOravec <James.Oravec@gmail.com>
2012-02-03 23:01:53 -0800

    init checkin

---
+++ src/edu/unlv/cs673/echoteam/project1/dao/JdbcDemo.java
@@ -0,0 +1,83 @@
+/**
+ * @author James Oravec
+ * CS673 - Project 1 - Echo Team
+ * 
+ * In order to use the following code, you need to add the mysql-connector-java.5.1.18-bin.jar file to your project class path.
+ * You can get the zip file, which contains the jar from: http://www.mysql.com/downloads/connector/j/
+ * 
+ * In order to use add the jar to your class path, do the following:
+ * 1) download the zip file.
+ * 2) Extract the zip into a folder
+ * 3) Create a "lib" directory under your Eclipse project
+ * 4) Copy the .jar file into the lib directory
+ * 5) Right click on your project -> Build Path -> Configure build path
+ * 6) Libraries Tab
+ * 7) Add Jars
+ * 8) Select the jar from you lib directory.
+ * 
+ * I got the base of the code below from: http://www.java-samples.com/showtutorial.php?tutorialid=9
+ * 
+ * I modified it to supply the username and password of the db. In order for this code to work, you'll need to of installed
+ * mysql and created the database and tables, as specified in a previous email.
+ * 
+ * Since we can get this working in java, we should be able to make it work easily with a servlet/jsp.
+ */
+
+package edu.unlv.cs673.echoteam.project1.dao;
+
+import java.sql.*;
+
+public class JdbcDemo{
+	static String dbUrl = "jdbc:mysql://localhost/echoTeam";
+	static String query = "Select * FROM echousers";
+	static String user = "root";
+	static String password = "password";
+	static Connection con = null;
+
+	public static void main(String args[]){
+		System.out.println("Start");
+		JdbcDemo jd = new JdbcDemo();
+		String result = jd.outputAllEchoUserRecords();
+		System.out.println("result: " + result);
+		System.out.println("End");
+	}
+	
+	/**
+	 * @author James Oravec
+	 * The following will connect to the MySQL db, query the user table
+	 * and output all of the information in the table.
+	 * 
+	 * @return
+	 */
+	public String outputAllEchoUserRecords(){
+		StringBuilder sb = new StringBuilder();
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection(dbUrl, user, password);
+			
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+
+			while (rs.next()) {
+				sb.append("(");
+				sb.append(rs.getString(1));
+				sb.append(",");
+				sb.append(rs.getString(2));
+				sb.append(",");
+				sb.append(rs.getString(3));
+				sb.append(") ");
+			}
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally{
+			try{
+				con.close();
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		return sb.toString();
+	}
+}
---
+++ src/edu/unlv/cs673/echoteam/servlet/jdbcDemoServlet.java
@@ -0,0 +1,48 @@
+package edu.unlv.cs673.echoteam.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import edu.unlv.cs673.echoteam.project1.dao.JdbcDemo;
+
+/**
+ * Servlet implementation class jdbcDemoServlet
+ */
+public class jdbcDemoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	private static JdbcDemo myDao = new JdbcDemo();
+
+	public void init() throws ServletException {
+		// Nothing needed.
+	}
+	
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+//		// Set a cookie for the user, so that the counter does not increate
+//		// everytime the user press refresh
+//		HttpSession session = request.getSession(true);
+//		// Set the session valid for 5 secs
+//		session.setMaxInactiveInterval(5);
+//		response.setContentType("text/plain");
+
+		PrintWriter out = response.getWriter();
+		out.println(myDao.outputAllEchoUserRecords());
+	}
+
+	public void destroy() {
+//		super.destroy();
+//		try {
+//			dao.save(count);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+	}
+
+}
